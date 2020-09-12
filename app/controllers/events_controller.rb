b3d647296c03adf895e21cfd46f6b4fc8a11bd16
Zhenya Telyukov <telyukov@gmail.com>
2020-09-12 21:55:48 +0300

    model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = [
-      {
-        title: "London Retail Expo",
-        datetime: "Monday 21 Oct, 2019",
-        location: "London Excel Centre"
-      },
-      {
-        title: "Enterprise Sales Training Workshop",
-        datetime: "Tuesday 22 Oct, 2019",
-        location: "Expert Sales Company Headquarters"
-      },
-      {
-        title: "Ruby Hack Night",
-        datetime: "Friday 25 Oct, 2019",
-        location: "Learnetto Headquarters"
-      },
-      {
-        title: "Beginners Salsa dance meetup",
-        datetime: "Saturday 26 Oct, 2019",
-        location: "Bar Salsa"
-      }
-    ]
+    @events = Event.order('start_datetime ASC')
   end
 end
