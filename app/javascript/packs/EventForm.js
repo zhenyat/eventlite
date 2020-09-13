6bdf758ce340d441f9a85d7ed7b4c4cae30c4749
Zhenya Telyukov <telyukov@gmail.com>
2020-09-13 16:11:57 +0300

    submit hamdling

--- app/javascript/packs/EventForm.js
+++ app/javascript/packs/EventForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 class EventForm extends Component {
   constructor(props) {
@@ -22,11 +23,29 @@ class EventForm extends Component {
     event.preventDefault()
   }
 
+  handleSubmit = e => {
+    axios({
+      method: 'POST',
+      url: '/events',
+      data: {event: this.state},
+      headers: {
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+      }
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    e.preventDefault()
+  }
+
   render() { 
     return (  
       <div>
         <h4>Create an Event:</h4>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' name='title' placeholder='Title' onChange={this.handleInput}/>
           <input type='text' name='start_datetime' placeholder='Date' onChange={this.handleInput}/>
           <input type='text' name='location' placeholder='Location' onChange={this.handleInput}/>
